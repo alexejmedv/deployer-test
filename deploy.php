1fc92d245de90085cb5cf7ea98b4454395b57615
Alexej Medvedev <alexej.medvedev@starfinanz.de>
2018-11-08 11:33:34 +0100

    deploy.php git_tty = false

--- deploy.php
+++ deploy.php
@@ -3,33 +3,47 @@ namespace Deployer;
 
 require 'recipe/common.php';
 
+
+
 // Project name
-set('application', 'deployer-test');
+set('application', '_DEPLOYER_TEST');
+
 
 // Project repository
-set('repository', 'git@github.com:alexejmedv/deployer-test.git');
+set('repository', 'https://github.com/alexejmedv/deployer-test.git');
+
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
+
 
 // Shared files/dirs between deploys 
 set('shared_files', ['README.md']);
 set('shared_dirs', ['src']);
 
+
 // Writable dirs by web server 
 set('writable_dirs', get('shared_dirs'));
 
 
-// ?!?
+// Keep releases -> dep rollback // for rolling back
+set('keep_releases', 3);
+
+
+// User the web server runs as
 set('http_user', 'www-data');
 
-
 // Hosts
+inventory('./hosts.yml');
+set('default_stage', 'development');
+
+/*
 host('localhost')
     ->user('root')
     ->port(2222)
-    ->set('deploy_path', '/deploy_path/{{application}}');
-    
+    ->stage('development')
+    ->set('deploy_path', '/{{application}}');
+*/    
 
 // Tasks
 desc('Deploy your project');
@@ -48,6 +62,11 @@ task('deploy', [
     'cleanup',
     'success'
 ]);
+
+
+
+
+
 
 // [Optional] If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
